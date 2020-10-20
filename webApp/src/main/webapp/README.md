2663be388cfde4fc7cc55d9bb9205a204aab7a76
Mokhira Atashikova <mokhira.atashikova@nu.edu.kz>
2020-10-20 16:16:20 +0600

    commit all files

---
+++ webApp/src/main/webapp/README.md
@@ -0,0 +1,8 @@
+# final_project_swe
+hotel chain web app
+
+
+Task for tomorrow, Thursday:
+- Add Design for Profile page 
+- Fix entry verification
+- Send ajax requests to register and get info from DB
---
+++ webApp/src/main/webapp/profile.js
@@ -0,0 +1,29 @@
+$('.nav ul li').click(function() {
+    $(this).addClass("active").siblings().removeClass("active");
+})
+
+const tabBtn = document.querySelector('.nav ul li');
+const tab = document.querySelectorAll('.tab');
+
+function tabs(panelIndex) {
+    tab.forEach(function(node) {
+        node.style.display = 'none';
+    });
+    tab[panelIndex].style.display = 'block';
+}
+
+tabs(0);
+
+let bio = document.querySelector('.bio');
+
+$('#delete1').click(function(){
+    $("div").remove("#d1");
+});
+
+$('#delete2').click(function(){
+    $("div").remove("#d2");
+});
+
+$('#delete3').click(function(){
+    $("div").remove("#d3");
+});
